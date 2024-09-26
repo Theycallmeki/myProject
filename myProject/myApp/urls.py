8ae084a7ed040017873cbbc0f98fe12f88d9c4b4
Theycallmeki <laxamana.kian13@gmail.com>
2024-09-26 22:23:04 +0800

    login register

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('contact/', views.contact, name='contact'),
+    path('main/', views.main, name='main'),
+
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -4,3 +4,41 @@ from django.shortcuts import render
 def home(request):
     context={}
     return render(request,'myApp/home.html',context)
+
+def contact(request):
+    context={}
+
+    return render(request, 'components/contact.html')
+def main(request):
+    context={}
+
+    return render(request, 'myApp/main.html', context)
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegistrationForm, LoginForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')  # Redirect to a home page after registration
+    else:
+        form = RegistrationForm()
+    return render(request, 'myApp/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main')  # Redirect to a home page after login
+    else:
+        form = LoginForm()
+    return render(request, 'myApp/login.html', {'form': form})
+
